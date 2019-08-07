1462165247709bb81ac1f8958b5eef8386488642
alexge50 <alexge50.x@gmail.com>
2019-08-07 04:15:35 +0300

    remade Chip8 implementation

--- Chip8/Chip8.h
+++ Chip8/Chip8.h
@@ -20,6 +20,11 @@
 #define CHIP8_EMULATOR_CHIP8_H
 
 #include <cstdint>
+#include <fstream>
+#include <string_view>
+#include <vector>
+
+#include "Chip8Instructions.h"
 
 const int CHIP8_NUMBER_OPCODES = 35;
 const int CHIP8_WIDTH = 64;
@@ -34,41 +39,91 @@ const int CHIP8_SPRITE_WIDTH = 8;
 const int CHIP8_MAX_VALUE = 255;
 const int CHIP8_MIN_VALUE = 0;
 
-struct Chip8Memory
-{
-    uint8_t memory[CHIP8_MEMORY];
-    uint8_t registers[CHIP8_REGISTERS];
+class Chip8;
 
-    uint16_t index, program_counter;
-
-    uint8_t graphics_memory[CHIP8_HEIGHT][CHIP8_WIDTH];
-
-    uint8_t delay_timer, sound_timer;
-
-    uint16_t stack[CHIP8_STACK_SIZE];
-    uint16_t stack_pointer;
-
-    uint8_t key[CHIP8_KEYS];
-};
-
-typedef void(*InstructionCallback)(uint16_t, Chip8Memory*);
-struct Chip8Instruction
+using InstructionCallback  = void (*)(uint16_t, Chip8&);
+struct InstructionEntry
 {
     uint16_t mask, value;
     InstructionCallback instruction_callback;
 };
 
-class Chip8 {
-public:
-    void Initialize();
-    void EmulateCycle();
-
-    uint8_t GetPixel(int line, int column);
-    void SetKey(int key);
-
-private:
-    Chip8Memory memory;
-    Chip8Instruction instructionTable[CHIP8_NUMBER_OPCODES];
+const InstructionEntry instructionTable[] =
+{
+        {0xF000, 0x0000, _0NNN},
+        {0xFFFF, 0x00E0, _00E0},
+        {0xFFFF, 0x00EE, _00EE},
+        {0xF000, 0x1000, _1NNN},
+        {0xF000, 0x2000, _2NNN},
+        {0xF000, 0x3000, _3XNN},
+        {0xF000, 0x4000, _4XNN},
+        {0xF00F, 0x5000, _5XY0},
+        {0xF000, 0x6000, _6XNN},
+        {0xF000, 0x7000, _7XNN},
+        {0xF00F, 0x8000, _8XY0},
+        {0xF00F, 0x8001, _8XY1},
+        {0xF00F, 0x8002, _8XY2},
+        {0xF00F, 0x8003, _8XY3},
+        {0xF00F, 0x8004, _8XY4},
+        {0xF00F, 0x8005, _8XY5},
+        {0xF00F, 0x8006, _8XY6},
+        {0xF00F, 0x8007, _8XY7},
+        {0xF00F, 0x800E, _8XYE},
+        {0xF00F, 0x9000, _9XY0},
+        {0xF000, 0xA000, _ANNN},
+        {0xF000, 0xB000, _BNNN},
+        {0xF000, 0xC000, _CXNN},
+        {0xF000, 0xD000, _DXYN},
+        {0xF0FF, 0xE09E, _EX9E},
+        {0xF0FF, 0xE0A1, _EXA1},
+        {0xF0FF, 0xF007, _FX07},
+        {0xF0FF, 0xF00A, _FX0A},
+        {0xF0FF, 0xF015, _FX15},
+        {0xF0FF, 0xF018, _FX18},
+        {0xF0FF, 0xF01E, _FX1E},
+        {0xF0FF, 0xF029, _FX29},
+        {0xF0FF, 0xF033, _FX33},
+        {0xF0FF, 0xF055, _FX55},
+        {0xF0FF, 0xF065, _FX65},
 };
+
+using Instruction = std::uint16_t;
+
+struct Chip8
+{
+    uint8_t memory[CHIP8_MEMORY] = {};
+    uint8_t registers[CHIP8_REGISTERS] = {};
+
+    uint16_t index = {}, program_counter = {};
+
+    uint8_t graphics_memory[CHIP8_HEIGHT][CHIP8_WIDTH] = {};
+
+    uint8_t delay_timer = {}, sound_timer = {};
+
+    uint16_t stack[CHIP8_STACK_SIZE] = {};
+    uint16_t stack_pointer = {};
+
+    uint8_t key[CHIP8_KEYS] = {};
+};
+
+static void tick(Chip8& chip)
+{
+    uint16_t opcode = (chip.memory[chip.program_counter] << 8u) + chip.memory[chip.program_counter + 1];
+
+    for(const auto& instruction: instructionTable)
+        if((opcode & instruction.mask) == instruction.value)
+            instruction.instruction_callback(opcode, chip);
+}
+
+static Chip8 loadRom(const char* filename)
+{
+    Chip8 chip;
+
+    std::ifstream romFile(filename, std::ios::binary | std::ios::in);
+    romFile.rdbuf()->sgetn(reinterpret_cast<char*>(chip.memory + 0x200), 0xfff - 0x200);
+    chip.program_counter = 0x200;
+
+    return chip;
+}
 
 #endif //CHIP8_EMULATOR_CHIP8_H
